eca0c4de286cf60e7c9df42569570fa367a12b56
Manan Papdiwala <Manan Papdiwala>
2016-11-02 00:00:14 -0400

    First Table

---
+++ Database/dbo_SpotLightProducts/Tables/tb_AppUser.sql
@@ -0,0 +1,15 @@
+CREATE TABLE tb_AppUser
+(
+    Id INTEGER, 
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50),
+    Email VARCHAR(100),
+    Pwd VARCHAR(100),
+    Is_Active BOOLEAN DEFAULT 1,
+    Created_On TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+    Created_By VARCHAR(100),
+    Modified_On TIMESTAMP,
+    Modified_By VARCHAR(100),
+    Version INTEGER DEFAULT 1,
+    CONSTRAINT df_tb_AppUser_Id PRIMARY KEY(Id)
+)
