dd3a22c8a4b21f90dfe617a473ee28f2cede017a
Sagar <savora@iu.edu>
2016-11-09 00:41:42 -0500

    changes in the structure

--- Database/dbo_SpotLightProducts/StoreProcedures/Dashboard/sp_update_User_Password.sql
+++ Database/dbo_SpotLightProducts/StoreProcedures/Dashboard/sp_update_User_Password.sql
@@ -1,15 +1,20 @@
 DELIMITER $$
 CREATE PROCEDURE sp_update_User_Password(
-IN p_userEmail VARCHAR(100),
+IN p_userID integer,
 IN p_currentPassword VARCHAR(100),
 IN p_newPassword VARCHAR(100)
 )
 BEGIN
 declare oldPassword varchar(100);
-declare userId varchar(100);
-Select pwd, Id into oldPassword, userId from tb_AppUser where Email = p_userEmail;
+SELECT
+  pwd INTO oldPassword
+FROM tb_AppUser
+WHERE id = p_userID
+AND Is_Active = 1;
 If oldPassword = p_currentPassword  THEN
-	Update tb_AppUser set Pwd = p_newPassword where Id = userId;
+	UPDATE tb_AppUser
+	SET Pwd = p_newPassword
+	WHERE Id = p_userID;
 	Select 1 as Success , "Password Successfully Changed" as Message;
 ELSE
 	Select -1 as Success, "Current Password Doesn't match with the entered one" as Message;
--- Database/dbo_SpotLightProducts/StoreProcedures/sp_get_All_Products_For_A_Seller_Below_Level.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER $$
-CREATE PROCEDURE sp_get_All_Products_For_A_Seller_Below_Level(
-IN p_seller_Id integer
-)
-BEGIN
-select p.Product_Name, p.Model_Id, p.Description, ps.price, ps.quantity from tb_products p inner join tb_productseller ps on p.Id = ps.Product_Id where ps.Seller_Id=5 and ps.quantity <= 5;
-End;
-$$
