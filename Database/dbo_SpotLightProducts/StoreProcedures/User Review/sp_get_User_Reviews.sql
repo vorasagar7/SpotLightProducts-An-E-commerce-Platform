27ceeed7c42c6b3ed3ff23dbf606fb543b67cd74
Sagar <savora@iu.edu>
2016-11-14 02:45:21 -0500

    modified for changes in query

--- Database/dbo_SpotLightProducts/StoreProcedures/User Review/sp_get_User_Reviews.sql
+++ Database/dbo_SpotLightProducts/StoreProcedures/User Review/sp_get_User_Reviews.sql
@@ -3,6 +3,24 @@ CREATE PROCEDURE sp_get_User_Reviews(
 IN p_user_Id integer
 )
 BEGIN
-select urv.User_Id, urv.Product_Id, urv.Comment, urv.Rating from tb_userreviews urv where urv.User_Id = p_user_Id;
+SELECT
+  urv.User_Id,
+  urv.Product_Id,
+  p.Product_Name,
+  p.Model_Id,
+  urv.modified_On,
+  urv.Comment,
+  urv.Rating,
+  brand.Name
+FROM tb_userreviews urv
+INNER JOIN tb_appuser ap
+  ON ap.id = urv.User_Id
+INNER JOIN tb_products p
+  ON p.Id = urv.Product_Id
+INNER JOIN tb_ReferenceListItem brand
+	ON brand.Id = p.Brand_Id
+WHERE urv.User_Id = p_user_Id
+AND ap.Is_Active = 1
+ORDER BY urv.modified_On DESC;
 End;
 $$
