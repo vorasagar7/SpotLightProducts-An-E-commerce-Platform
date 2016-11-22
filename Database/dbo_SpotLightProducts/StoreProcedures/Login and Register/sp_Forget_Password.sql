42555c8ba42f30f84a346e0ea99f45c6549d8f72
Sagar <savora@iu.edu>
2016-11-21 20:40:59 -0500

    forget password

---
+++ Database/dbo_SpotLightProducts/StoreProcedures/Login and Register/sp_Forget_Password.sql
@@ -0,0 +1,12 @@
+DELIMITER $$
+CREATE PROCEDURE sp_Forget_Password(
+In p_Email Varchar(100)
+)
+BEGIN
+	SELECT  
+		pwd 
+    FROM tb_AppUser 
+    WHERE email = p_Email
+	AND Is_DELETED = 0;
+End;
+$$
