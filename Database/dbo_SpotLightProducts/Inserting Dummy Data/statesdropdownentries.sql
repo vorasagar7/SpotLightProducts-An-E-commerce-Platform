b5c48f4ac50038f3cdffd8daded528abd267fd49
Sagar <savora@iu.edu>
2016-11-23 19:10:58 -0500

    changes for states dropdown

---
+++ Database/dbo_SpotLightProducts/Inserting Dummy Data/statesdropdownentries.sql
@@ -0,0 +1,105 @@
+insert into tb_referencelist (Name,Created_By,Modified_On,Modified_By) values ('States',CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Alaska',4,1,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Alabama',4,2,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Arkansas',4,3,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Arizona',4,4,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('California',4,5,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Colorado',4,6,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Connecticut',4,7,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('District of Columbia',4,8,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Delaware',4,9,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Florida',4,10,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Georgia',4,11,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Hawaii',4,12,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Iowa',4,13,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Idaho',4,14,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Illinois',4,15,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Indiana',4,16,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Kansas',4,17,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Kentucky',4,18,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Louisiana',4,19,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Massachusetts',4,20,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Maryland',4,21,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Maine',4,22,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Michigan',4,23,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Minnesota',4,24,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Missouri',4,25,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Mississippi',4,26,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Montana',4,27,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('North Carolina',4,28,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('North Dakota',4,29,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Nebraska',4,30,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('New Hampshire',4,31,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('New Jersey',4,32,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('New Mexico',4,33,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Nevada',4,34,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('New York',4,35,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Ohio',4,36,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Oklahoma',4,37,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Oregon',4,38,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Pennsylvania',4,39,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Puerto Rico',4,40,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Rhode Island',4,41,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('South Carolina',4,42,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('South Dakota',4,43,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Tennessee',4,44,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Texas',4,45,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Utah',4,46,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Virginia',4,47,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Vermont',4,48,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Washington',4,49,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Wisconsin',4,50,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('West Virginia',4,51,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
+
+insert into tb_referencelistitem(Name,List_Id,Sort_Order,Created_By,Modified_On,Modified_By) values ('Wyoming',4,52,CURRENT_USER,CURRENT_TIMESTAMP,CURRENT_USER);
