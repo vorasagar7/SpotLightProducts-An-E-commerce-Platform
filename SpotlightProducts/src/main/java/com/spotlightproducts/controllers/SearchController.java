cfbd880a34e4c07d61f5e25a7a92d803f598908a
Ronak Parekh <parekhr@indiana.edu>
2016-11-26 01:17:58 -0500

    Search feature integrated and Product Server Side implemented

---
+++ SpotlightProducts/src/main/java/com/spotlightproducts/controllers/SearchController.java
@@ -0,0 +1,34 @@
+package com.spotlightproducts.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.spotlightproducts.dao.*;
+import com.spotlightproducts.controllers.*;;
+
+@Controller
+public class SearchController {
+
+	@Autowired
+	SearchService searchService;
+	
+	@RequestMapping(value = "/search", method=RequestMethod.POST)
+	public String searchProducts(@RequestParam String searchQuery, ModelMap model){
+		
+		ArrayList<Product> productList = searchService.getProductList(searchQuery);
+		
+		model.addAttribute(productList);
+		
+		model.put("productList",productList);
+		
+		System.out.println("Seach Query is: "+searchQuery);
+		return "/SearchPageViews/search-result";
+		
+	}	
+}
