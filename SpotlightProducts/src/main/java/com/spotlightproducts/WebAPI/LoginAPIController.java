02c9bbaecb485cd095b67cd35dc89693cb8c3b9a
Ronak Parekh <parekhr@indiana.edu>
2016-11-27 16:35:10 -0500

    Session attribute added

--- SpotlightProducts/src/main/java/com/spotlightproducts/WebAPI/LoginAPIController.java
+++ SpotlightProducts/src/main/java/com/spotlightproducts/WebAPI/LoginAPIController.java
@@ -5,25 +5,31 @@ import javax.mail.internet.AddressException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.spotlightproducts.dao.DatabaseResponse;
 import com.spotlightproducts.dao.JSONResponse;
 import com.spotlightproducts.dao.User;
 
 @RestController
+@SessionAttributes("email")	//this annotation is used for Session Handling in Spring MVC. The parameters i.e "email" will be stored in the model object till the end of the session OR till User logout OR till 30 minutes whichever happens first
 public class LoginAPIController{
 	
 	@RequestMapping(value = "/AuthenticateUser", method = RequestMethod.POST)
-	public ResponseEntity<JSONResponse<String>> AuthenticateUser(@RequestBody User user){
+	public ResponseEntity<JSONResponse<String>> AuthenticateUser(@RequestBody User user, ModelMap model){
 		boolean isValidUser = user.validateUserDao();
+		model.put("email",user.getEmail());
 		JSONResponse JsonResponse = new JSONResponse(); 
 		if(isValidUser){
 			JsonResponse.setStatus("Success");
+			//model.put("email",email);
 		}
 		else{
 			JsonResponse.setStatus("Failure");
