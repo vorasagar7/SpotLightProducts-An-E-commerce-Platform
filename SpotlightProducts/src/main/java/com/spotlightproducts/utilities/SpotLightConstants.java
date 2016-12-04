58249572d8ca323fd6e8fd41ca1118240a7ad005
Sagar <savora@iu.edu>
2016-12-04 15:44:45 -0500

    constant added

--- SpotlightProducts/src/main/java/com/spotlightproducts/utilities/SpotLightConstants.java
+++ SpotlightProducts/src/main/java/com/spotlightproducts/utilities/SpotLightConstants.java
@@ -62,7 +62,8 @@ public class SpotLightConstants {
 	public static final String SP_GET_USER_ID = "{call sp_User_ID_Get(?)}";
 	public static final String SP_GET_SELLERAPPROVED_PRODUCT = "{call sp_Seller_ApprovedProducts_Get(?)}";
 	public static final String SP_SELLER_DELETE_PRODUCT = "{call sp_Seller_Delete_Product(?,?)}"; 
-	public static final String SP_INVENTORY_MANAGEMENT_MODIFY_QUANTITY = "{call sp_Inventory_Management_Modify_Quantity(?,?,?,?)}"; 
+	public static final String SP_INVENTORY_MANAGEMENT_MODIFY_QUANTITY = "{call sp_Inventory_Management_Modify_Quantity(?,?,?,?)}";
+	public static final String SP_BRAND_CATEGORY_REFERENCE_DATA = "{call sp_Brand_Category_ReferenceData_Get()}"; 
 
 	// Constant Methods
 	
